971fe59c34aa742208d8aceec065d5ec19e18b51
Richard Johnston <techpowerawaits@outlook.com>
2024-04-22 17:07:41 -0600

    Add sword to gamegrid
    For testing purposes.

--- scene/gamegrid.gd
+++ scene/gamegrid.gd
@@ -1,6 +1,7 @@
 extends TileMap
 
 var player_scene = preload("res://scene/player.tscn")
+var sword_scene = preload("res://scene/sword.tscn")
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -13,7 +14,10 @@ func _ready():
 	var screen_size = get_viewport_rect().size
 	add_child(player)
 	player.spawn(position, position.y, screen_size.y - 32, position.x, screen_size.x - 32)
-
+	
+	var sword = sword_scene.instance()
+	add_child(sword)
+	sword.position = Vector2(position.x + 32, position.y + 32)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
