8dc595f9e7d78f8a8e806dc89e6c9b1cd7533ff0
Richard Johnston <techpowerawaits@outlook.com>
2024-04-22 18:31:29 -0600

    Add zombies to gamegrid
    For testing purposes. One zombie currently doesn't show up.

--- scene/gamegrid.gd
+++ scene/gamegrid.gd
@@ -2,7 +2,8 @@ extends TileMap
 
 var player_scene = preload("res://scene/player.tscn")
 var sword_scene = preload("res://scene/sword.tscn")
-var item_ref_counter = {}
+var zombie_scene = preload("res://scene/zombie.tscn")
+var ref_counter = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,7 +18,20 @@ func _ready():
 	var sword = sword_scene.instance()
 	add_child(sword)
 	sword.position = Vector2(position.x + 32, position.y + 32)
-	item_ref_counter["sword"] = 1
+	ref_counter["sword"] = 1
+	
+	var zombie1 = zombie_scene.instance()
+	var zombie2 = zombie_scene.instance()
+	var zombie3 = zombie_scene.instance()
+	
+	add_child(zombie1)
+	add_child(zombie2)
+	add_child(zombie3)
+	
+	zombie1.position = Vector2(position.x + 64, position.y + 32)
+	zombie2.position = Vector2(position.x, position.y + 64)
+	zombie3.position = Vector2(position.x + 32, position.y + 32)
+	ref_counter["zombie"] = 3
 
 func _on_player_pick_up_item(item_name):
-	item_ref_counter[item_name] -= 1
+	ref_counter[item_name] -= 1
