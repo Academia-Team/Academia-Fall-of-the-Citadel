f9903c2e65eedbfdfd38bda529e1424919dc6ab7
Richard Johnston <techpowerawaits@outlook.com>
2024-04-24 17:56:48 -0600

    Cleanup infobar.gd

--- scene/infobar.gd
+++ scene/infobar.gd
@@ -5,15 +5,8 @@ var score
 func _ready():
 	score = 0
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_player_pick_up_item(item_name):
 	$item_info.text = item_name
-
 
 func _on_player_used_item(_item_name):
 	$item_info.text = ""
