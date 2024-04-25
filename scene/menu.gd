e9b102aadfeb79ffea68f43237cd0bcbd7911181
Richard Johnston <techpowerawaits@outlook.com>
2024-04-25 17:53:02 -0600

    Enable entering game from menu

--- scene/menu.gd
+++ scene/menu.gd
@@ -1,7 +1,12 @@
 extends ColorRect
 
+const game_scene = preload("res://scene/gamescr.tscn")
+
 func _ready():
 	$enter_button.grab_focus()
+
+func _on_enter_button_pressed():
+	get_tree().change_scene_to(game_scene)
 
 func _on_perish_button_pressed():
 	get_tree().notification(MainLoop.NOTIFICATION_WM_QUIT_REQUEST)
