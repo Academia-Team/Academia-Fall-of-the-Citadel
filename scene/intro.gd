5aa5df0d109508a0fc05ed39d20f5f888cb98bfd
Richard Johnston <techpowerawaits@outlook.com>
2024-04-25 18:47:54 -0600

    Allow intro animation to be skipped

--- scene/intro.gd
+++ scene/intro.gd
@@ -11,15 +11,24 @@ func _ready():
 	fontRGBVal = 0
 	fontColor = Color8(fontRGBVal, fontRGBVal, fontRGBVal)
 	$Label.add_color_override("font_color", getDesiredColor())
+	
+# Allow for skipping the animation.
+func _unhandled_input(event):
+	if event is InputEventKey or \
+		event is InputEventGesture or \
+		event is InputEventJoypadButton or \
+		event is InputEventJoypadMotion or \
+		event is InputEventMouseButton:
+		$fadetimer.emit_signal("timeout")
 
-func getDesiredColor():
-	return Color8(fontRGBVal, fontRGBVal, fontRGBVal)
-
-func _process(delta):
+func _process(_delta):
 	if not shouldFadeLabelOut:
 		fadeLabelIn()
 	else:
 		fadeLabelOut()
+
+func getDesiredColor():
+	return Color8(fontRGBVal, fontRGBVal, fontRGBVal)
 
 func fadeLabelIn():
 	if fontRGBVal <= 0xFF:
