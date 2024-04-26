5219438599068c11d781aabf2269f1ba2008cdd9
Richard Johnston <techpowerawaits@outlook.com>
2024-04-26 15:10:46 -0600

    Add default text for item label

--- scene/infobar.gd
+++ scene/infobar.gd
@@ -1,15 +1,17 @@
 extends ColorRect
 
+var orig_item_text = ""
 var score
 
 func _ready():
 	score = 0
 
 func _on_player_pick_up_item(item_name):
+	orig_item_text = $item_info.text
 	$item_info.text = item_name
 
 func _on_player_used_item(_item_name):
-	$item_info.text = ""
+	$item_info.text = orig_item_text
 
 func _on_player_health_change(lives):
 	$lives_counter.text = "Lives: %d" % lives
