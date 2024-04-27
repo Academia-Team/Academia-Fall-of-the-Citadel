aed7197fdbe16032b26eaf53571f01db9772c7a3
Richard Johnston <techpowerawaits@outlook.com>
2024-04-27 11:57:13 -0600

    Replace placeholder images with zombie sprites

--- scene/zombie.gd
+++ scene/zombie.gd
@@ -1,9 +1,15 @@
 extends Area2D
 class_name enemy
 
+const direction = preload("res://class/direction.gd")
+
+var cur_orient
+
 signal enemy_destroyed(enemy_type)
 
 func get_class():
+	cur_orient = direction.SOUTH
+	
 	return "enemy"
 
 # Called when the node enters the scene tree for the first time.
@@ -15,3 +21,16 @@ func attack():
 	$collisionbox.set_deferred("monitorable", false)
 	emit_signal("enemy_destroyed", get_meta("type"))
 	$Sprite.self_modulate = Color.tomato
+
+func set_orient(orient):
+	orient = direction.get_horz_component(orient)
+	match orient:
+		direction.NORTH:
+			$Sprite.set_texture(load("res://asset/Zombie_NORTH.png"))
+		direction.SOUTH:
+			$Sprite.set_texture(load("res://asset/Zombie_SOUTH.png"))
+		_:
+			$Sprite.set_texture(load("res://asset/Zombie_SIDE.png"))
+			
+	$Sprite.flip_h = (orient == direction.WEST)
+	cur_orient = orient
