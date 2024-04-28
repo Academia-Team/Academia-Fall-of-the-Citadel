67e0ae5408c506d4cfbe4fc65473dcbfd81bc58d
Richard Johnston <techpowerawaits@outlook.com>
2024-04-27 19:01:12 -0600

    Stop zombie from moving after death

--- scene/zombie.gd
+++ scene/zombie.gd
@@ -4,11 +4,14 @@ class_name enemy
 signal enemy_destroyed(enemy_type)
 signal move_request(ref)
 
+var alive
+
 func get_class():
 	return "enemy"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	alive = true
 	set_meta("type", "zombie")
 	
 	hide()
@@ -16,10 +19,12 @@ func _ready():
 	$collisionbox.set_deferred("monitorable", false)
 
 func attack():
+	alive = false
 	$collisionbox.set_deferred("monitoring", false)
 	$collisionbox.set_deferred("monitorable", false)
 	emit_signal("enemy_destroyed", get_meta("type"))
 	$CharacterSprite.show_hurt()
+	$move_timer.stop()
 	
 func desired_positions(target_pos):
 	var dir = Direction.get_dir_facing(target_pos, position)
@@ -28,15 +33,17 @@ func desired_positions(target_pos):
 	
 	var possible_positions = []
 	
-	for direction in component_directions:
-		possible_positions.append(Direction.translate_pos(position, direction, 32))
+	if alive:
+		for direction in component_directions:
+			possible_positions.append(Direction.translate_pos(position, direction, 32))
 	
 	return possible_positions
 
 func move(pos):
-	var diff_pos = pos - position
-	position = pos
-	$CharacterSprite.set_orient(Direction.rel_pos_to_dir(diff_pos))
+	if alive:
+		var diff_pos = pos - position
+		position = pos
+		$CharacterSprite.set_orient(Direction.rel_pos_to_dir(diff_pos))
 
 func spawn(pos, orient):
 	position = pos
@@ -45,7 +52,9 @@ func spawn(pos, orient):
 	show()
 	$collisionbox.set_deferred("monitoring", true)
 	$collisionbox.set_deferred("monitorable", true)
+	$move_timer.start()
 
 
 func _on_move_timer_timeout():
-	emit_signal("move_request", self)
+	if alive:
+		emit_signal("move_request", self)
