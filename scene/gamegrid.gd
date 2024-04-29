3c9675e6fa6320c1b61fcfbff827145152c0ab43
Richard Johnston <techpowerawaits@outlook.com>
2024-04-28 18:55:56 -0600

    Create start() method in game grid

--- scene/gamegrid.gd
+++ scene/gamegrid.gd
@@ -21,6 +21,9 @@ signal score_change(score_diff)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	start()
+
+func start():
 	if seed_val == null:
 		seed_val = hash(Time.get_datetime_dict_from_system())
 	
