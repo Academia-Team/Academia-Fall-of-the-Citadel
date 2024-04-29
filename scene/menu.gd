340ce0ddf3bbf5c8a5c3ea7fbb0dca13022ff9c5
Richard Johnston <techpowerawaits@outlook.com>
2024-04-28 20:33:05 -0600

    Another attempt

--- scene/menu.gd
+++ scene/menu.gd
@@ -28,28 +28,36 @@ func _on_enter_button_gui_input(event):
 		enter_seed = true
 		$Option.show()
 		$Option/option_label.text = "Seed:"
-		$Option/option_enter.grab_focus()
+		$Option/option_text.grab_focus()
 
 
-func _on_option_enter_breakpoint_toggled(_row):
-	if enter_seed:
-		enter_seed = false
-		var seed_str = $Option/option_enter.text
-		seed_str.rstrip("\r\n")
-		
-		var seed_val = 0
-		
-		if seed_str.is_valid_integer():
-			seed_val = seed_str.to_int()
-		else:
-			seed_val = hash(seed_str)
-			
-		var game_instance = game_scene.instance()
-		game_instance.seed_val = seed_val
-		
-		add_child(game_instance)
-
-
-func _on_option_enter_focus_exited():
+func _on_option_text_focus_exited():
 	enter_seed = false
+	$Option/option_label.text = "Option:"
+	$Option/option_text.text = ""
 	$Option.hide()
+
+
+func _on_option_text_gui_input(event):
+	if event.is_action("ui_accept", true):
+		enter_seed = false
+		
+		if len($Option/option_text.text) == 0:
+			$enter_button.grab_focus()
+		else:
+			enter_seeded_game($Option/option_text.text)
+	else:
+		$Option/option_text.text += event.as_text()
+
+func enter_seeded_game(seed_str):
+	var seed_val = 0
+		
+	if seed_str.is_valid_integer():
+		seed_val = seed_str.to_int()
+	else:
+		seed_val = hash(seed_str)
+		
+	var game_instance = game_scene.instance()
+	game_instance.seed_val = seed_val
+	
+	add_child(game_instance)
