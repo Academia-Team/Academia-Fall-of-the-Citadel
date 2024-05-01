48fb6b4d6637a297d1b32238e766e748ccef4a76
Richard Johnston <techpowerawaits@outlook.com>
2024-05-01 15:12:38 -0600

    Group buttons together in GameOver screen

--- scene/gameover.gd
+++ scene/gameover.gd
@@ -10,41 +10,41 @@ func _ready():
 
 func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
-		$give_up_button.emit_signal("pressed")
+		$Buttons/GiveUp.emit_signal("pressed")
 	elif Input.is_action_just_pressed("ui_focus_next") and get_focus_owner() == null:
-		$enter_button.grab_focus()
+		$Buttons/Arise.grab_focus()
 
 func start(info_obj):
 	info_ref = info_obj
 	_set_seed_text(info_obj.get_seed())
 	show()
-	$revive_button.grab_focus()
+	$Buttons/Arise.grab_focus()
 
 func _set_seed_text(seed_val):
 	if seed_val != null:
-		$seed.text = "Seed: %d" % seed_val
+		$Seed.text = "Seed: %d" % seed_val
 
 func _on_gameover_draw():
 	if info_ref != null:
-		$score.text = info_ref.get_score_text()
+		$Score.text = info_ref.get_score_text()
 
-func _on_give_up_button_pressed():
+func _on_GiveUp_pressed():
 	emit_signal("leave")
 
-func _on_revive_button_pressed():
+func _on_Arise_pressed():
 	emit_signal("retry")
 
-func _on_revive_button_mouse_entered():
-	$revive_button.grab_focus()
+func _on_Arise_mouse_entered():
+	$Buttons/Arise.grab_focus()
 
 
-func _on_revive_button_mouse_exited():
-	$revive_button.release_focus()
+func _on_Arise_mouse_exited():
+	$Buttons/Arise.release_focus()
 
 
-func _on_give_up_button_mouse_entered():
-	$give_up_button.grab_focus()
+func _on_GiveUp_mouse_entered():
+	$Buttons/GiveUp.grab_focus()
 
 
-func _on_give_up_button_mouse_exited():
-	$give_up_button.release_focus()
+func _on_GiveUp_mouse_exited():
+	$Buttons/GiveUp.release_focus()
