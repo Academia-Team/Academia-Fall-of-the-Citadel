15854bc9a3309ab3758778e5179b9628a970dd13
Richard Johnston <techpowerawaits@outlook.com>
2024-05-01 08:05:46 -0600

    Remove use of signals for score management

--- scene/gameover.gd
+++ scene/gameover.gd
@@ -20,17 +20,13 @@ func start(info_obj, seed_val):
 	show()
 	$revive_button.grab_focus()
 
-func _set_score(score_val):
-	if score_val != null:
-		$score.text = "Score: %d" % score_val
-
 func _set_seed(seed_val):
 	if seed_val != null:
 		$seed.text = "Seed: %d" % seed_val
 
 func _on_gameover_draw():
 	if info_ref != null:
-		_set_score(info_ref.score)
+		$score.text = info_ref.get_score_text()
 
 func _on_give_up_button_pressed():
 	emit_signal("leave")
--- scene/infobar.gd
+++ scene/infobar.gd
@@ -1,10 +1,31 @@
 extends ColorRect
 
 var orig_item_text = ""
-var score
+var _score
 
 func _ready():
-	score = 0
+	_score = 0
+
+func incr_score(score_delta):
+	_score += score_delta
+	_write_score_text()
+
+func decr_score(score_delta):
+	if score_delta > _score:
+		_score = 0
+	else:
+		_score -= score_delta
+	
+	_write_score_text()
+
+func _write_score_text():
+	$score_counter.text = "Score: %d" % _score
+
+func get_score():
+	return _score
+
+func get_score_text():
+	return $score_counter.text
 
 func _on_player_pick_up_item(item_name):
 	orig_item_text = $item_info.text
@@ -15,7 +36,3 @@ func _on_player_used_item(_item_name):
 
 func _on_player_health_change(lives):
 	$lives_counter.text = "Lives: %d" % lives
-
-func _on_gamegrid_score_change(score_diff):
-	score += score_diff
-	$score_counter.text = "Score: %d" % score
