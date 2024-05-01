eaf6857cf5283197d513dd3b52b954efb1e7cb65
Richard Johnston <techpowerawaits@outlook.com>
2024-05-01 15:38:24 -0600

    Hide cursor while switching buttons using keyboard
    Avoids some graphical oddities. Also includes a bugfix.

--- scene/gameover.gd
+++ scene/gameover.gd
@@ -2,6 +2,9 @@ extends ColorRect
 
 var info_ref = null
 
+var ignore_mouse_warp = false
+var mouse_over = null
+
 signal leave()
 signal retry()
 
@@ -11,8 +14,17 @@ func _ready():
 func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
 		$Buttons/GiveUp.emit_signal("pressed")
-	elif Input.is_action_just_pressed("ui_focus_next") and get_focus_owner() == null:
-		$Buttons/Arise.grab_focus()
+	elif Input.is_action_just_pressed("ui_focus_next"):
+		if get_focus_owner() == null:
+			$Buttons/Arise.grab_focus()
+		
+		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+			
+			if mouse_over != null:
+				warp_mouse(get_global_mouse_position() - Vector2(0, mouse_over.rect_size.y))
+				ignore_mouse_warp = true
+
 
 func start(info_obj):
 	info_ref = info_obj
@@ -35,16 +47,27 @@ func _on_Arise_pressed():
 	emit_signal("retry")
 
 func _on_Arise_mouse_entered():
+	mouse_over = $Buttons/Arise
 	$Buttons/Arise.grab_focus()
 
 
 func _on_Arise_mouse_exited():
+	mouse_over = null
 	$Buttons/Arise.release_focus()
 
 
 func _on_GiveUp_mouse_entered():
+	mouse_over = $Buttons/GiveUp
 	$Buttons/GiveUp.grab_focus()
 
 
 func _on_GiveUp_mouse_exited():
+	mouse_over = null
 	$Buttons/GiveUp.release_focus()
+
+
+func _on_GameOver_gui_input(event):
+	if event is InputEventMouseMotion and Input.mouse_mode != Input.MOUSE_MODE_VISIBLE:
+		if not ignore_mouse_warp:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		ignore_mouse_warp = false
