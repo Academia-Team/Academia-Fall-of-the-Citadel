d2ebb55bd40f0782abeaf40f4d1172f91639f0df
Richard Johnston <techpowerawaits@outlook.com>
2024-05-01 10:04:27 -0600

    Use LineEdit for the menu options
    Fixes #34.

--- scene/menu.gd
+++ scene/menu.gd
@@ -14,7 +14,6 @@ func _process(_delta):
 		$perish_button.emit_signal("pressed")
 	elif Input.is_action_just_pressed("ui_focus_next") and get_focus_owner() == null:
 		$enter_button.grab_focus()
-		print("Drat")
 
 func _on_enter_button_pressed():
 	game_playing = true
@@ -29,41 +28,41 @@ func _on_enter_button_gui_input(event):
 		enter_seed = true
 		$Option.show()
 		$Option/option_label.text = "Seed:"
-		$Option/option_enter.grab_focus()
+		$Option/option_line.grab_focus()
 
 
-func _on_option_enter_focus_exited():
+func _on_option_line_focus_exited():
 	enter_seed = false
-	$Option/option_enter.text = ""
+	$Option/option_line.text = ""
 	$Option/option_label.text = "Option:"
 	$Option.hide()
 
-
-func _on_option_enter_text_changed():
-	if $Option/option_enter.text.ends_with("\n"):
-		enter_seed = false
-		var seed_str = $Option/option_enter.text
-		seed_str = seed_str.strip_escapes()
-		
-		var seed_val = 0
-		
-		if seed_str.is_valid_integer():
-			seed_val = seed_str.to_int()
-		else:
-			seed_val = hash(seed_str)
-		
-		var game_instance = SceneSwitcher.get_scene(SceneSwitcher.GAME).instance()
-		game_instance.seed_val = seed_val
-		
-		self_modulate.a = 0
-		$Option/option_enter.release_focus()
-		call_deferred("add_child", game_instance)
-		game_playing = true
+func _on_option_line_text_entered(new_text):
+	enter_seed = false
+	
+	var seed_val = 0
+	
+	if new_text.is_valid_integer():
+		seed_val = new_text.to_int()
+	else:
+		seed_val = hash(new_text)
+	
+	var game_instance = SceneSwitcher.get_scene(SceneSwitcher.GAME).instance()
+	game_instance.seed_val = seed_val
+	
+	self_modulate.a = 0
+	$Option/option_line.release_focus()
+	call_deferred("add_child", game_instance)
+	game_playing = true
 
 
-func _on_option_enter_gui_input(event):
+func _on_option_line_gui_input(event):
 	if event.is_action("ui_cancel", true):
 		$enter_button.grab_focus()
+
+
+func _on_option_line_text_change_rejected(_rejected_substring):
+	$Option/reject.play()
 
 
 func _on_enter_button_mouse_entered():
