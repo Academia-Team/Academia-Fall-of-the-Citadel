0c1fc146016d3e01f4c5a3439457d459e63d72cc
Richard Johnston <techpowerawaits@outlook.com>
2024-05-02 17:17:41 -0600

    Increase scroll speed

--- scene/credit.gd
+++ scene/credit.gd
@@ -1,5 +1,7 @@
 extends ColorRect
 
+const SCROLL_SPEED = 16
+
 func _ready():
 	$ScrollContainer.grab_focus()
 
@@ -7,6 +9,6 @@ func _ready():
 func _on_ScrollContainer_gui_input(event):
 	var curr_vscroll_val = $ScrollContainer.get_v_scroll()
 	if event.is_action("ui_scroll_down", true):
-		$ScrollContainer.set_v_scroll(curr_vscroll_val + 1)
+		$ScrollContainer.set_v_scroll(curr_vscroll_val + SCROLL_SPEED)
 	if event.is_action("ui_scroll_up", true):
-		$ScrollContainer.set_v_scroll(curr_vscroll_val - 1)
+		$ScrollContainer.set_v_scroll(curr_vscroll_val - SCROLL_SPEED)
