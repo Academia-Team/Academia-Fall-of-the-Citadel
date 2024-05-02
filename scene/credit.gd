a953b4ac9faeb10be070dafd757fead28dba1bdb
Richard Johnston <techpowerawaits@outlook.com>
2024-05-02 14:21:00 -0600

    Allow credits to be scrolled via keyboard

--- scene/credit.gd
+++ scene/credit.gd
@@ -1,4 +1,12 @@
 extends ColorRect
 
 func _ready():
-	$Body.grab_focus()
+	$ScrollContainer.grab_focus()
+
+
+func _on_ScrollContainer_gui_input(event):
+	var curr_vscroll_val = $ScrollContainer.get_v_scroll()
+	if event.is_action("ui_scroll_down", true):
+		$ScrollContainer.set_v_scroll(curr_vscroll_val + 1)
+	if event.is_action("ui_scroll_up", true):
+		$ScrollContainer.set_v_scroll(curr_vscroll_val - 1)
