1565c5578f0e8f5049ec4bca356f7b56daf6f135
Richard Johnston <techpowerawaits@outlook.com>
2024-05-09 09:18:11 -0600

    Remove template comments from sword

--- scene/sword.gd
+++ scene/sword.gd
@@ -1,15 +1,10 @@
 extends Area2D
 class_name item
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 func get_class():
 	return "item"
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	set_meta("type", "sword")
 
