05e03276c64fe473835a5eec5646e5518f728efe
Richard Johnston <techpowerawaits@outlook.com>
2024-06-20 15:10:12 -0600

    Improve code quality of class/Direction.gd

--- class/Direction.gd
+++ class/Direction.gd
@@ -1,5 +1,5 @@
-extends Reference
 class_name Direction
+extends Reference
 
 enum { NORTH, SOUTH, EAST, WEST, NORTHEAST, NORTHWEST, SOUTHEAST, SOUTHWEST }
 
@@ -49,43 +49,37 @@ static func dir_to_rel_pos(dir, step):
 static func get_horz_component(dir):
 	if dir == NORTHEAST or dir == SOUTHEAST:
 		return EAST
-	elif dir == NORTHWEST or dir == SOUTHWEST:
+	if dir == NORTHWEST or dir == SOUTHWEST:
 		return WEST
-	else:
-		return dir
+	return dir
 
 
 static func get_vert_component(dir):
 	if dir == NORTHEAST or dir == NORTHWEST:
 		return NORTH
-	elif dir == SOUTHWEST or dir == SOUTHEAST:
+	if dir == SOUTHWEST or dir == SOUTHEAST:
 		return SOUTH
-	else:
-		return dir
+	return dir
 
 
 static func combine_dir(dir1, dir2):
 	if dir1 == null:
 		return dir2
-	elif dir2 == null:
+	if dir2 == null:
 		return dir1
-	elif dir_opposites(dir1, dir2):
+	if dir_opposites(dir1, dir2):
 		return null
-	elif dir1 != dir2:
+	if dir1 != dir2:
 		if dir1 == NORTH:
 			if dir2 == EAST:
 				return NORTHEAST
-			else:
-				return NORTHWEST
-		elif dir1 == SOUTH:
+			return NORTHWEST
+		if dir1 == SOUTH:
 			if dir2 == EAST:
 				return SOUTHEAST
-			else:
-				return SOUTHWEST
-		else:
-			return combine_dir(dir2, dir1)
-	else:
-		return dir1
+			return SOUTHWEST
+		return combine_dir(dir2, dir1)
+	return dir1
 
 
 static func dir_opposites(dir1, dir2):
@@ -128,15 +122,12 @@ static func get_cardinal_dir_facing(pos_to_face, pos):
 		if abs(pos.x - pos_to_face.x) < abs(pos.y - pos_to_face.y) or (pos.y - pos_to_face.y) == 0:
 			if (pos.x - pos_to_face.x) >= 0:
 				return WEST
-			else:
-				return EAST
+			return EAST
 		else:
 			if (pos.y - pos_to_face.y) >= 0:
 				return NORTH
-			else:
-				return SOUTH
-	else:
-		return null
+			return SOUTH
+	return null
 
 
 static func get_dir_facing(pos_to_face, pos):
@@ -150,19 +141,17 @@ static func rel_pos_to_dir(pos):
 static func _get_horz_dir_from_pos(pos):
 	if pos.x > 0:
 		return EAST
-	elif pos.x < 0:
+	if pos.x < 0:
 		return WEST
-	else:
-		return null
+	return null
 
 
 static func _get_vert_dir_from_pos(pos):
 	if pos.y > 0:
 		return SOUTH
-	elif pos.y < 0:
+	if pos.y < 0:
 		return NORTH
-	else:
-		return null
+	return null
 
 
 static func is_cardinal_dir(dir):
