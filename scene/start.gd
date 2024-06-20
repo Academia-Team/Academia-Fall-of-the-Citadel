42c130a72b979de82615025c59547554f41274ea
Richard Johnston <techpowerawaits@outlook.com>
2024-06-20 17:12:21 -0600

    Format scene/start.gd

--- scene/start.gd
+++ scene/start.gd
@@ -5,6 +5,7 @@ func _ready():
 	if OS.get_name() != "HTML5":
 		SceneSwitcher.change_scene_tree_to(get_tree(), SceneSwitcher.INTRO)
 
+
 func _process(_delta):
 	if Input.is_action_pressed("ui_advance"):
 		SceneSwitcher.change_scene_tree_to(get_tree(), SceneSwitcher.INTRO)
