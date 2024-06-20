3ae2073ba7ce5076543437b2734f64bbb71c28ca
Richard Johnston <techpowerawaits@outlook.com>
2024-06-20 17:06:53 -0600

    Format scene/intro.gd

--- scene/intro.gd
+++ scene/intro.gd
@@ -1,14 +1,16 @@
 extends ColorRect
 
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	$AnimationPlayer.play("Fade In")
-	
+
+
 # Allow for skipping the animation.
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_advance"):
 		var next_anim = $AnimationPlayer.animation_get_next($AnimationPlayer.current_animation)
-		
+
 		$AnimationPlayer.emit_signal("animation_finished", $AnimationPlayer.current_animation)
 		$AnimationPlayer.play(next_anim)
 
