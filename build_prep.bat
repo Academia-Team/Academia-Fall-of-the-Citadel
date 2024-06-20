395b207528115a9458d12f20387aed2304a4a087
Richard Johnston <techpowerawaits@outlook.com>
2024-06-20 17:03:50 -0600

    Fix build_prep.bat

--- build_prep.bat
+++ build_prep.bat
@@ -1,9 +1,9 @@
 rd /s /q build
 
 md build
-md build/linux64
-md build/linux86
-md build/web
-md build/web-main
-md build/win64
-md build/win86
+md build\linux64
+md build\linux86
+md build\web
+md build\web-main
+md build\win64
+md build\win86
