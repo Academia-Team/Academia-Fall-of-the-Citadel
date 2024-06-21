2a483ed7dddcb6b76fb90bee0a8e08960e391230
Richard Johnston <techpowerawaits@outlook.com>
2024-06-21 15:11:00 -0600

    Have alert go off when no changes in text can be made

--- scene/Menu.gd
+++ scene/Menu.gd
@@ -6,6 +6,8 @@ var ignore_mouse_warp = false
 var mouse_over = null
 var seed_val = null
 
+var text_changed = false
+
 
 func _ready():
 	$Buttons/Enter.grab_focus()
@@ -96,6 +98,7 @@ func _on_SeedDialog_line_text_entered(new_text):
 
 func _on_SeedDialog_line_text_change_rejected(_rejected_substring):
 	$SeedDialog/Reject.play()
+	text_changed = true
 
 
 func _on_SeedDialog_hide():
@@ -200,3 +203,12 @@ func _on_Duck_mouse_entered():
 func _on_Duck_mouse_exited():
 	mouse_over = null
 	$ModeDialog/Buttons/Duck.release_focus()
+
+
+func _on_SeedDialog_line_text_changed(_new_text):
+	text_changed = true
+
+
+func _on_SeedDialog_line_gui_input(_event):
+	if Input.is_action_just_pressed("ui_cntrl"):
+		$SeedDialog/Reject.play()
