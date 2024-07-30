626bd54516d3313034255d71a07ac62447b0efad
Richard Johnston <techpowerawaits@outlook.com>
2024-07-29 20:30:31 -0600

    Have GameOver utilize FocusedButton

--- scene/GameOver.gd
+++ scene/GameOver.gd
@@ -5,25 +5,9 @@ signal retry
 
 var info_ref = null
 
-var ignore_mouse_warp = false
-var mouse_over = null
-
 
 func _ready():
 	hide()
-
-
-func _process(_delta):
-	if Input.is_action_just_pressed("ui_focus_next"):
-		if get_focus_owner() == null:
-			$Buttons/Arise.grab_focus()
-
-		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
-			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-
-			if mouse_over != null:
-				warp_mouse(get_global_mouse_position() - Vector2(0, mouse_over.rect_size.y))
-				ignore_mouse_warp = true
 
 
 func start(info_obj):
@@ -40,8 +24,6 @@ func start(info_obj):
 
 func stop():
 	info_ref = null
-	ignore_mouse_warp = false
-	mouse_over = null
 	release_focus()
 	hide()
 
@@ -67,30 +49,3 @@ func _on_GiveUp_pressed():
 
 func _on_Arise_pressed():
 	emit_signal("retry")
-
-
-func _on_Arise_mouse_entered():
-	mouse_over = $Buttons/Arise
-	$Buttons/Arise.grab_focus()
-
-
-func _on_Arise_mouse_exited():
-	mouse_over = null
-	$Buttons/Arise.release_focus()
-
-
-func _on_GiveUp_mouse_entered():
-	mouse_over = $Buttons/GiveUp
-	$Buttons/GiveUp.grab_focus()
-
-
-func _on_GiveUp_mouse_exited():
-	mouse_over = null
-	$Buttons/GiveUp.release_focus()
-
-
-func _on_GameOver_gui_input(event):
-	if event is InputEventMouseMotion and Input.mouse_mode != Input.MOUSE_MODE_VISIBLE:
-		if not ignore_mouse_warp:
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		ignore_mouse_warp = false
