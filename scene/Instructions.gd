7a771c3e065b15c3e491af627abf4da1c8a3bfe4
Richard Johnston <techpowerawaits@outlook.com>
2024-07-31 15:31:42 -0600

    Improve code quality of scene/Instructions.gd
    Static typing is used and the number of pages is no longer hardcoded.

--- scene/Instructions.gd
+++ scene/Instructions.gd
@@ -3,57 +3,53 @@ extends ColorRect
 signal finished
 signal switched_to_page(page_num)
 
-const NUM_PAGES = 8
-
-var curr_page = null
-var page_counter = 0
+var _curr_page: CanvasItem
+var _page_counter: int
 
 
-func _ready():
-	hide_all_pages()
-	handle_page_request()
+func _ready() -> void:
+	start()
 
 
-func _process(_delta):
-	if Input.is_action_just_pressed("ui_advance"):
-		handle_page_request()
+func start() -> void:
+	stop()
+	_handle_page_request()
 
 
-func get_next_page():
-	var next_page = null
-	page_counter += 1
-
-	if page_counter <= NUM_PAGES:
-		var page_node_name = "Pg%d" % page_counter
-		next_page = get_node(page_node_name)
-
-	return next_page
+func stop() -> void:
+	get_tree().call_group_flags(SceneTree.GROUP_CALL_REALTIME, "Page", "hide")
+	_curr_page = null
+	_page_counter = -1
 
 
-func handle_page_request():
-	var next_page = get_next_page()
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_advance") and _curr_page != null:
+		_handle_page_request()
+
+
+func get_next_page() -> CanvasItem:
+	var page: CanvasItem = null
+	var pages: Array = get_tree().get_nodes_in_group("Page")
+	var next_page_count: int = _page_counter + 1
+
+	if next_page_count < pages.size():
+		page = pages[next_page_count]
+
+	return page
+
+
+func _handle_page_request() -> void:
+	var next_page: CanvasItem = get_next_page()
 
 	if next_page != null:
-		if curr_page != null:
-			curr_page.hide()
+		if _curr_page != null:
+			_curr_page.hide()
 
 		next_page.show()
-		curr_page = next_page
+		_curr_page = next_page
+		_page_counter += 1
 
-		emit_signal("switched_to_page", page_counter)
+		emit_signal("switched_to_page", _page_counter)
 	else:
 		emit_signal("finished")
 		SceneSwitcher.change_scene_tree_to(get_tree(), SceneSwitcher.MENU)
-
-
-func hide_all_pages():
-	var children = get_children()
-
-	for child in children:
-		if child.name.begins_with("Pg"):
-			child.hide()
-
-
-func reset():
-	curr_page = null
-	page_counter = 0
