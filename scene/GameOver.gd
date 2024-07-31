69eb734c2cde959d04264d0d6a6396610fbc3c34
Richard Johnston <techpowerawaits@outlook.com>
2024-07-31 14:31:00 -0600

    Utilize ButtonGridContainer in GameOver
    Also use static typing in GameOver.

--- scene/GameOver.gd
+++ scene/GameOver.gd
@@ -3,47 +3,46 @@ extends ColorRect
 signal leave
 signal retry
 
-var info_ref = null
+var info_ref: InfoBar = null
 
 
 func _ready():
 	hide()
+	($Buttons as ButtonGridContainer).disable_buttons()
 
 
-func start(info_obj):
+func start(info_obj: InfoBar) -> void:
 	info_ref = info_obj
 	_set_seed_text(info_obj.get_seed())
 	_set_mode_text(info_obj.get_mode())
 
 	if not info_obj.is_tainted():
 		$Tainted.hide()
+	
+	($Buttons as ButtonGridContainer).enable_buttons()
 	show()
 
-	$Buttons/Arise.grab_silent_focus()
 
-
-func stop():
+func stop() -> void:
 	info_ref = null
-	release_focus()
+	($Buttons as ButtonGridContainer).disable_buttons()
 	hide()
 
 
-func _set_seed_text(seed_val):
-	if seed_val != null:
-		$Seed.text = "Seed: %d" % seed_val
+func _set_seed_text(seed_val: int) -> void:
+	$Seed.text = "Seed: %d" % seed_val
 
 
-func _set_mode_text(mode):
-	if mode != null:
-		$Mode.text = "Mode: %s" % mode
+func _set_mode_text(mode: String) -> void:
+	$Mode.text = "Mode: %s" % mode
 
 
-func _on_GameOver_draw():
+func _on_GameOver_draw() -> void:
 	if info_ref != null:
 		$Score.text = "Score: %d" % info_ref.get_score()
 
 
-func _on_GiveUp_button_effects_finished():
+func _on_GiveUp_button_effects_finished() -> void:
 	emit_signal("leave")
 
 
