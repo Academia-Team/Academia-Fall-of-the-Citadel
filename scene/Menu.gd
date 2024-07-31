531a8958c9d2d7aacf235f587988c44c6ffa2ea5
Richard Johnston <techpowerawaits@outlook.com>
2024-07-31 11:07:26 -0600

    Split out seed dialog from menu (#96)
    * Remove usage of set_process()
    
    Not needed in Menu.
    
    * Perform initial separation of SeedDialog as LineDialog
    
    * Improve formatting of class/LineDialog.gd
    
    * Prevent buttons from triggering when leaving SeedDialog
    
    * Have scene/Menu.gd use static typing
    
    * Improve formatting of scene/Menu.gd

--- scene/Menu.gd
+++ scene/Menu.gd
@@ -1,18 +1,15 @@
 extends ColorRect
 
-const MAX_INT_LEN = 19
-
-var seed_val = null
-
-var text_changed = false
+var seed_val: int = 0
+var seed_val_set: bool = false
 
 
-func _ready():
+func _ready() -> void:
 	$Buttons/Enter.grab_silent_focus()
 	$Version.text = get_version_str()
 
 
-func get_version_str():
+func get_version_str() -> String:
 	var version_str = ProjectSettings.get_setting("global/Version")
 
 	if ProjectSettings.get_setting("global/Dev"):
@@ -21,104 +18,82 @@ func get_version_str():
 	return version_str
 
 
-func _disable_menu_buttons():
+func _disable_menu_buttons() -> void:
 	$Buttons/Enter.disabled = true
 	$Buttons/HelpMe.disabled = true
 	$Buttons/Credit.disabled = true
 	$Buttons/Perish.disabled = true
 
 
-func _enable_menu_buttons():
+func _enable_menu_buttons() -> void:
 	$Buttons/Enter.disabled = false
 	$Buttons/HelpMe.disabled = false
 	$Buttons/Credit.disabled = false
 	$Buttons/Perish.disabled = false
 
 
-func _on_Enter_button_effects_finished():
+func _on_Enter_button_effects_finished() -> void:
 	_disable_menu_buttons()
 	$Buttons.hide()
 	$ModeDialog.show_modal()
 	$ModeDialog/Buttons/Regular.grab_silent_focus()
 
 
-func _activate_game(mode):
+func _activate_game(mode: String) -> void:
 	$ModeDialog.hide()
 	_disable_menu_buttons()
 
-	var game_instance = SceneSwitcher.get_scene(SceneSwitcher.GAME).instance()
-	game_instance.seed_val = seed_val
+	var game_instance: Control = SceneSwitcher.get_scene(SceneSwitcher.GAME).instance()
+	if seed_val_set:
+		game_instance.seed_val = seed_val
+
 	game_instance.mode = mode
 	self_modulate.a = 0
 
 	call_deferred("add_child", game_instance)
-	set_process(false)
 	$Buttons/Enter.release_focus()
 
 
-func _on_Perish_button_effects_finished():
+func _on_Perish_button_effects_finished() -> void:
 	SceneSwitcher.change_scene_tree_to(get_tree(), SceneSwitcher.QUIT)
 
 
-func _on_Enter_gui_input(event):
+func _on_Enter_gui_input(event: InputEvent) -> void:
 	if not $Buttons/Enter.disabled and event.is_action("button_options", true):
-		$SeedDialog.show_modal()
-		$SeedDialog/HBoxContainer/Line.grab_focus()
-		set_process(false)
+		_disable_menu_buttons()
+		($SeedDialog as LineDialog).prompt_integer("Seed:")
 
 
-func _on_SeedDialog_line_text_entered(new_text):
-	seed_val = 0
-
-	if new_text.is_valid_integer() and new_text.length() <= MAX_INT_LEN + int(new_text[0] == "-"):
-		seed_val = new_text.to_int()
-	else:
-		seed_val = hash(new_text)
-
-	$SeedDialog.hide()
-	$SeedDialog.emit_signal("popup_hide")
-
-
-func _on_SeedDialog_line_text_change_rejected(_rejected_substring):
-	$SeedDialog/Reject.play()
-	text_changed = true
-
-
-func _on_SeedDialog_line_text_changed(_new_text):
-	text_changed = true
-
-
-func _on_SeedDialog_line_gui_input(_event):
-	if Input.is_action_just_pressed("ui_cntrl") and not text_changed:
-		$SeedDialog/Reject.play()
-
-	text_changed = false
-
-
-func _on_SeedDialog_hide():
-	$SeedDialog/HBoxContainer/Line.text = ""
-	$Buttons/Enter.grab_silent_focus()
-	set_process(true)
-	_enable_menu_buttons()
-
-
-func _on_HelpMe_button_effects_finished():
+func _on_HelpMe_button_effects_finished() -> void:
 	SceneSwitcher.change_scene_tree_to(get_tree(), SceneSwitcher.HELP)
 
 
-func _on_Credit_button_effects_finished():
+func _on_Credit_button_effects_finished() -> void:
 	SceneSwitcher.change_scene_tree_to(get_tree(), SceneSwitcher.CREDIT)
 
 
-func _on_ModeDialog_hide():
+func _on_ModeDialog_hide() -> void:
 	_enable_menu_buttons()
 	$Buttons/Enter.grab_silent_focus()
 	$Buttons.show()
 
 
-func _on_Regular_button_effects_finished():
+func _on_Regular_button_effects_finished() -> void:
 	_activate_game("Regular")
 
 
-func _on_Duck_button_effects_finished():
+func _on_Duck_button_effects_finished() -> void:
 	_activate_game("Duck")
+
+
+func _on_SeedDialog_text_rejected() -> void:
+	($Alert as AnimationPlayer).play()
+
+
+func _on_SeedDialog_integer_prompt_finished(text_entered: bool, value: int) -> void:
+	if text_entered:
+		seed_val = value
+		seed_val_set = true
+
+	# Delay the re-enabling of buttons to ensure that they don't accidently activate.
+	call_deferred("_enable_menu_buttons")
