52ce7f53e1be53882c565158af71812683010662
Richard Johnston <techpowerawaits@outlook.com>
2024-07-31 18:55:21 -0600

    Add ability to disable button in ButtonGridContainer

--- class/ButtonGridContainer.gd
+++ class/ButtonGridContainer.gd
@@ -1,12 +1,14 @@
 class_name ButtonGridContainer
 extends FocusedGridContainer
 
+var _disabled_buttons: Array = []
+
 
 func enable_buttons() -> void:
 	var children: Array = get_children()
 
 	for child in children:
-		if child is BaseButton:
+		if child is BaseButton and not child.name in _disabled_buttons:
 			child.disabled = false
 
 
@@ -16,3 +18,10 @@ func disable_buttons() -> void:
 	for child in children:
 		if child is BaseButton:
 			child.disabled = true
+
+
+func hide_and_disable(button_name: String) -> void:
+	var target_button: BaseButton = get_node(button_name)
+	target_button.disabled = true
+	target_button.hide()
+	_disabled_buttons.append(button_name)
