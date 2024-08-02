98bbee4546ad85ae24f9ba5c780a6108f6b1a7cf
Richard Johnston <techpowerawaits@outlook.com>
2024-08-01 18:04:30 -0600

    Add Menu to Intro

--- scene/Intro.gd
+++ scene/Intro.gd
@@ -1,5 +1,7 @@
 extends ColorRect
 
+var finish_intro: bool = false
+
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
@@ -8,7 +10,7 @@ func _ready():
 
 # Allow for skipping the animation.
 func _process(_delta):
-	if Input.is_action_just_pressed("ui_advance"):
+	if not finish_intro and Input.is_action_just_pressed("ui_advance"):
 		var next_anim = $AnimationPlayer.animation_get_next($AnimationPlayer.current_animation)
 
 		$AnimationPlayer.emit_signal("animation_finished", $AnimationPlayer.current_animation)
@@ -17,6 +19,6 @@ func _process(_delta):
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "Fade Out":
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		finish_intro = true
 		$Sound.stop()
-		SceneSwitcher.change_scene_tree_to(get_tree(), SceneSwitcher.MENU)
+		($Menu as Menu).enable()
