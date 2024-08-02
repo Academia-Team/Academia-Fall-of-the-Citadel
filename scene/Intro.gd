a6fb3306cf757bd517b847adb9f438538b781d88
Richard Johnston <techpowerawaits@outlook.com>
2024-08-01 20:28:11 -0600

    Improve scene/Intro.gd

--- scene/Intro.gd
+++ scene/Intro.gd
@@ -3,22 +3,23 @@ extends ColorRect
 var finish_intro: bool = false
 
 
-func _ready():
+func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-	$AnimationPlayer.play("Fade In")
+	($AnimationPlayer as AnimationPlayer).play("Fade In")
 
 
 # Allow for skipping the animation.
-func _process(_delta):
+func _process(_delta: float) -> void:
 	if not finish_intro and Input.is_action_just_pressed("ui_advance"):
-		var next_anim = $AnimationPlayer.animation_get_next($AnimationPlayer.current_animation)
-
-		$AnimationPlayer.emit_signal("animation_finished", $AnimationPlayer.current_animation)
-		$AnimationPlayer.play(next_anim)
+		_advance_to_end()
 
 
-func _on_AnimationPlayer_animation_finished(anim_name):
+func _advance_to_end() -> void:
+	$AnimationPlayer.advance(99999)
+
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	if anim_name == "Fade Out":
 		finish_intro = true
-		$Sound.stop()
+		($Sound as AudioStreamPlayer).stop()
 		($Menu as Menu).enable()
