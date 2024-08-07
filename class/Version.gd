5e6fd4b54587694f26334d37b3311e72a9fac87a
Richard Johnston <techpowerawaits@outlook.com>
2024-08-06 18:28:07 -0600

    Add Version class
    Hides the complexity of getting the version information.

---
+++ class/Version.gd
@@ -0,0 +1,29 @@
+class_name Version
+extends Reference
+
+const VER_PATH: String = "res://VERSION.txt"
+
+var _ver: String = ""
+
+
+func _ready() -> void:
+	var ver_file = File.new()
+
+	if ver_file.file_exists(VER_PATH):
+		var status: int = ver_file.open(VER_PATH, File.READ)
+
+		if status == OK:
+			_ver = ver_file.get_line()
+			ver_file.close()
+		else:
+			printerr('Failed to open "%s".' % VER_PATH)
+	else:
+		printerr('File "%s" does not exist.' % VER_PATH)
+
+
+func get_version() -> String:
+	return _ver
+
+
+func is_dev() -> bool:
+	return ProjectSettings.get_setting("global/Dev")
