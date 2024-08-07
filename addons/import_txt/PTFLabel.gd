0845d53d68516995be6786d61e44f4dad5ec7fc8
Richard Johnston <techpowerawaits@outlook.com>
2024-08-06 18:12:05 -0600

    Revert "Get initial version of import_txt"
    This reverts commit 0757a1abf3e6397a976a8db494959c0b91a90a64.

--- addons/import_txt/PTFLabel.gd
+++
@@ -1,8 +0,0 @@
-tool
-extends Label
-
-export var text_file: Resource = null
-
-
-func _on_enter_tree() -> void:
-	text = text_file.text
--- addons/import_txt/import_txt.gd
+++ addons/import_txt/import_txt.gd
@@ -1,19 +1,10 @@
 tool
 extends EditorPlugin
 
-var importer
-#var importer: EditorImportPlugin
-
 
 func _enter_tree() -> void:
-	add_custom_type("PlainTextFile", "Resource", preload("PlainTextFile.gd"), preload("icon.png"))
-	add_custom_type("PTFLabel", "Label", preload("PTFLabel.gd"), preload("icon.png"))
-	importer = preload("import.gd").new()
-	add_import_plugin(importer)
+	pass
 
 
 func _exit_tree() -> void:
-	remove_import_plugin(importer)
-	importer = null
-	remove_custom_type("PTFLabel")
-	remove_custom_type("PlainTextFile")
+	pass
