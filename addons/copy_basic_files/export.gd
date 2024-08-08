ce6f2b129f954a0b03cf04e8dce9025c01c3305a
Richard Johnston <techpowerawaits@outlook.com>
2024-08-07 18:04:48 -0600

    Add plugin that copies licensing information into export folders

---
+++ addons/copy_basic_files/export.gd
@@ -0,0 +1,31 @@
+tool
+extends EditorExportPlugin
+
+const DESIRED_FILES_PROPERTY: String = "global/files_to_add_on_export"
+const TARGET_FEATURE: String = "directly_viewable"
+
+var target_dir: String = ""
+
+
+func _export_begin(features: PoolStringArray, _is_debug: bool, path: String, _flags: int) -> void:
+	if TARGET_FEATURE in features:
+		target_dir = path.get_base_dir()
+
+
+func _export_end() -> void:
+	if ProjectSettings.has_setting(DESIRED_FILES_PROPERTY) and not target_dir.empty():
+		var project_dir: Directory = Directory.new()
+		var project_dir_status: int = project_dir.open("res://")
+
+		if project_dir_status == OK:
+			var abs_project_dir: String = project_dir.get_current_dir()
+			for filename in ProjectSettings.get_setting(DESIRED_FILES_PROPERTY):
+				var source_path: String = "%s/%s" % [abs_project_dir, filename]
+				var dest_path: String = "%s/%s" % [target_dir, filename]
+				var copy_status: int = project_dir.copy(source_path, dest_path)
+
+				if copy_status != OK:
+					printerr('Failed to copy "%s" to "%s".' % [filename, target_dir])
+					break
+		else:
+			printerr("Cannot open project directory.")
