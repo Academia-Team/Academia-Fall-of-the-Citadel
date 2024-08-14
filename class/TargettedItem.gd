f39d9e7be67321dda756a386e05aaed41d7d2e06
Richard Johnston <techpowerawaits@outlook.com>
2024-08-13 19:43:07 -0600

    Add TargettedItem
    For those items that act on a bunch of targets.

---
+++ class/TargettedItem.gd
@@ -0,0 +1,12 @@
+class_name TargettedItem
+extends Item
+
+var targets: Array = [] setget set_targets, get_targets
+
+
+func set_targets(value: Array) -> void:
+	targets = value
+
+
+func get_targets() -> Array:
+	return targets
