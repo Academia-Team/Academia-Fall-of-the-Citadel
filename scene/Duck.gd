0ad144ddb05880f2adc090f3fc6e5a224bdce0eb
Richard Johnston <techpowerawaits@outlook.com>
2024-08-14 14:52:35 -0600

    Finally fix Duck

--- scene/Duck.gd
+++ scene/Duck.gd
@@ -4,12 +4,6 @@ extends Item
 var _duck_quacked: bool = false
 
 
-func ready() -> void:
-	var use_connect: int = $UseSFX.connect("finished", self, "_on_UseSFX_finished")
-	if use_connect != OK:
-		printerr("%s will never report a successful use." % type)
-
-
 func _use() -> void:
 	if not _duck_quacked:
 		$UseSFX.play()
