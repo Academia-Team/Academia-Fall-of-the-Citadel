047915ebeedbe7256b52f9ce61ec412dae12e0b5
Richard Johnston <techpowerawaits@outlook.com>
2024-08-16 16:14:47 -0600

    Add Event system
    Should make it easier to customize aspects of the game depending on certain
    conditions.

--- class/TileWorld.gd
+++ class/TileWorld.gd
@@ -3,6 +3,36 @@
 class_name TileWorld
 extends TileMap
 
+signal message_change_request(text, duration)
+signal music_change_request(stream, duration)
+
+const EVENT_NO_LIMIT: int = -1
+
+
+class Event:
+	extends Reference
+
+	var num_times: int
+	var max_times: int
+
+	var message: String
+	var music: AudioStream
+
+	func _init(text: String, max_occurences: int = EVENT_NO_LIMIT, song: AudioStream = null):
+		num_times = 0
+		max_times = max_occurences
+
+		message = text
+		music = song
+
+
+func send_event(event: Event, duration: float = EVENT_NO_LIMIT) -> void:
+	if event.max_times == EVENT_NO_LIMIT or event.num_times < event.max_times:
+		event.num_times += 1
+		emit_signal("message_change_request", event.message, duration)
+		if event.music != null:
+			emit_signal("music_change_request", event.music, duration)
+
 
 func pos_in_world(pos: Vector2) -> bool:
 	return get_cellv(world_to_map(pos)) != INVALID_CELL
