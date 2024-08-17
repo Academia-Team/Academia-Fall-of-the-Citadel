5536cc76b1852eb0441c3a80162e276b4565d8fc
Richard Johnston <techpowerawaits@outlook.com>
2024-08-17 13:13:28 -0600

    Add initial Mega Duck Event code
    Doesn't trigger much in game yet, but it is a start.

--- class/EventDefs.gd
+++ class/EventDefs.gd
@@ -1,5 +1,5 @@
 class_name EventDefs
 
-enum { P_INIT_MOV, P_INIT_PICK, P_DEATH }
+enum { P_INIT_MOV, P_INIT_PICK, P_DEATH, D_READY, D_MEGA }
 
 const NUM_EVENTS := 0
--- scene/Duck.gd
+++ scene/Duck.gd
@@ -1,16 +1,85 @@
 tool
 extends Item
 
+const DUCK_HOLDER_META := "duck_count"
+
+const MEGA_DUCK_READY := "Mega Duck Primed..."
+const MEGA_DUCK_READY_TIME := 3.0
+
+const MEGA_DUCK_ACTIVE := "Duck Power!"
+
+export var mega_duck_color := Color.gold setget set_mega_duck_color, get_mega_duck_color
+export var mega_duck_music: AudioStream setget set_mega_duck_music, get_mega_duck_music
+export var mega_duck_time := 3.492 setget set_mega_duck_time, get_mega_duck_time
+export var mega_duck_uses := 20 setget set_mega_duck_uses, get_mega_duck_uses
+
 var _duck_quacked := false
+
+
+func set_mega_duck_color(value: Color) -> void:
+	mega_duck_color = value
+
+
+func get_mega_duck_color() -> Color:
+	return mega_duck_color
+
+
+func set_mega_duck_music(value: AudioStream) -> void:
+	mega_duck_music = value
+
+
+func get_mega_duck_music() -> AudioStream:
+	return mega_duck_music
+
+
+func set_mega_duck_time(value: float) -> void:
+	if value > 0:
+		mega_duck_time = value
+
+
+func get_mega_duck_time() -> float:
+	return mega_duck_time
+
+
+func set_mega_duck_uses(value: int) -> void:
+	if value > 0:
+		mega_duck_uses = value
+
+
+func get_mega_duck_uses() -> int:
+	return mega_duck_uses
+
+
+func _acquire() -> void:
+	if not holder.has_meta(DUCK_HOLDER_META) or holder.get_meta(DUCK_HOLDER_META) >= mega_duck_uses:
+		holder.set_meta(DUCK_HOLDER_META, 0)
+
+	if holder.get_meta(DUCK_HOLDER_META) == (mega_duck_uses - 1):
+		var ready_event := TileWorld.Event.new(EventDefs.D_READY, MEGA_DUCK_READY)
+		gameworld.send_event(ready_event, MEGA_DUCK_READY_TIME)
 
 
 func _use() -> void:
 	if not _duck_quacked:
-		$UseSFX.play()
-		_duck_quacked = true
+		holder.set_meta(DUCK_HOLDER_META, holder.get_meta(DUCK_HOLDER_META) + 1)
+
+		if holder.get_meta(DUCK_HOLDER_META) == mega_duck_uses:
+			var active_event := TileWorld.Event.new(
+				EventDefs.D_MEGA, MEGA_DUCK_ACTIVE, 1, mega_duck_music, mega_duck_color
+			)
+			gameworld.send_event(active_event, mega_duck_time)
+			emit_signal("used")
+		else:
+			$UseSFX.play()
+			_duck_quacked = true
 	else:
 		emit_signal("failed_use")
 
 
 func _on_UseSFX_finished() -> void:
 	emit_signal("used")
+
+
+func _on_holder_existence_changed(value: bool) -> void:
+	if not value and holder.has_meta(DUCK_HOLDER_META):
+		holder.remove_meta(DUCK_HOLDER_META)
